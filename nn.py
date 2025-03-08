57e9e8ebf1a68c5bd976bbad1dd3ac2f6a6fad6d
Aditya Kumar <adityak2117@gmail.com>
2025-03-08 21:19:20 +0530

    Implemented neural network with feed forward pass functionality and tested it

--- nn.py
+++ nn.py
@@ -22,18 +22,10 @@ class Layer:
         Layer's weights initialization method to use.
     `activation`: `str`, ["identity", "sigmoid", "tanh", "relu", "softmax"],  default = "identity"
         Specify which activation function to use in forward pass
-    
-    Methods
-    -------
-    `forward(x: NDArray)`: `NDArray`
-        Takes the input and passes through this layer to give activated output
-
-
     """
     _weight_init_methods = ["random", "xavier"]
-    _activation_choices = ["identity", "sigmoid", "tanh", "relu", "softmax"]
 
-    def __init__(self, index: int, input_size: int, output_size: int, weight_init = "random", activation="identity"):
+    def __init__(self, index: int, input_size: int, output_size: int, weight_init = "random", activation = "identity"):
 
         assert (index >= 1), "Layer index should be greater than or equal to 1"
         assert (input_size >= 1), "Input side of the layer needs to have atleast one neuront"
@@ -42,9 +34,6 @@ class Layer:
         
         if weight_init not in self._weight_init_methods:
             raise ValueError(f"Unknown weight initialization method '{weight_init}', choose from: {self._weight_init_methods}")
-        
-        if activation not in self._activation_choices:
-            raise ValueError(f"Unknown activation choice '{activation}', choose from: {self._activation_choices}")
 
         self.index = index
         self.weight_init = weight_init
@@ -79,7 +68,87 @@ class Layer:
             return (W, b)
     
     def forward(self, x: npt.NDArray) -> npt.NDArray:
+        """
+        Takes the input `x` and passes through this layer to give activated output `h`.
+
+        Parameters
+        ----------
+        `x`: `NDArray`
+            The input vector to feed to the layer.
+        
+        Returns
+        -------
+        `NDArray`
+            The activated output of the layer after forward pass of `x`.
+        """
+        self.x = x
         self.a = (self.W @ x) + self.b
         self.h = self.activation.function(self.a)
         return self.h
 
+class NeuralNetwork:
+    """
+    Implements multi-layered neural network with flat layers.
+
+    - Can specify all the layers in one go.
+    - Or iteratively add new layers in front of the last layer.
+
+    Attributes
+    ----------
+    `weight_init`: `str`, ["random", "xavier"], default = "random"
+        All layers' weights initialization method to use.
+    `layer_sizes`: `list`, default = None
+        A list of strictly positive integers specifying number of neurons in each layer of the network. Can leave this empty if intend to use
+    `activations`: `list`, default = None
+        A list of activation function name strings to use for all the activation layers (hidden + output)
+    """
+    def __init__(self, weight_init = "random", layer_sizes: list = None, activations:list = None):
+        self.weight_init = weight_init
+        self.layers = []
+        
+        if layer_sizes is None:
+            print("Neural Network initialized without layers. Use add_layer() method to add layers sequentially.")
+        else:
+            assert (len(layer_sizes)) == (len(activations) + 1), "Provided number of layers does not have sufficient number of activation functions specified."
+
+            self.layers = [Layer(index = i+1,
+                                 input_size = layer_sizes[i],
+                                 output_size = layer_sizes[i+1],
+                                 weight_init=self.weight_init,
+                                 activation = activations[i])
+                                 for i in range(len(activations))]
+
+    def add_layer(self, input_size: int, output_size: int, weight_init = "random", activation = "identity"):
+        if self.layers is None:
+            self.layers[Layer(index=1,
+                              input_size=input_size,
+                              output_size=output_size,
+                              weight_init=weight_init,
+                              activation=activation)]
+        else:
+            last_layer_index = self.layers[-1].index
+            self.layers.append(Layer(index=last_layer_index+1,
+                                     input_size=input_size,
+                                     output_size=output_size,
+                                     weight_init=weight_init,
+                                     activation=activation))
+    
+    def forward(self, x: npt.NDArray) -> npt.NDArray:
+        """
+        Perform forward pass of `x` through the neural network.
+
+        Takes the input `x` and passes through all of the neural network layers to come up with final output.
+
+        Parameters
+        ----------
+        `x`: `NDArray`
+            The input vector to feed to the network.
+        
+        Returns
+        -------
+        `NDArray`
+            The activated output of the network after forward pass of `x`.
+        """
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
