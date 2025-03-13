fixed backpropagation, added utils module for data handling, cleaned optimizers a bit, a fix in sigmoid activation function

activations.py
@@ -21,15 +21,19 @@ class Activation():
         
         self.choice = choice
         self.function = self.identity
+        self.grad = self.identity_grad
         
         if self.choice == "softmax":
             self.function = self.softmax
         elif self.choice == "sigmoid":
             self.function = self.sigmoid
+            self.grad = self.sigmoid_grad
         elif self.choice == "tanh":
             self.function = self.tanh
+            self.grad = self.tanh_grad
         elif self.choice == "relu":
             self.function = self.relu
+            self.grad = self.relu_grad
 
     
     def identity(self, x: float | npt.NDArray) -> float | npt.NDArray:
@@ -62,7 +66,9 @@ class Activation():
         `float | NDArray`
             Activation output.
         """
-        return 1.0/(1.0 + np.exp(-x))
+        return np.where(x>=0,
+                        1.0/(1.0 + np.exp(-x)),
+                        np.exp(x)/(1.0 + np.exp(x)))
 
     def tanh(self, x: float | npt.NDArray) -> float | npt.NDArray:
         """

nn.py
@@ -1,6 +1,9 @@
 import numpy as np
 import numpy.typing as npt
 from activations import Activation
+from optimizers import Optimizer
+from collections import deque
+import losses
 
 class Layer:
     """
@@ -102,7 +105,10 @@ class NeuralNetwork:
     `activations`: `list`, default = None
         A list of activation function name strings to use for all the activation layers (hidden + output)
     """
-    def __init__(self, weight_init = "random", layer_sizes: list = None, activations:list = None):
+    def __init__(self, weight_init = "random", layer_sizes: list = None, activations:list = None, optimizer = "sgd", learning_rate = 0.001, **kwargs):
+        self.opt_name = optimizer
+        self.lr = learning_rate
+        self.optimizer_params = kwargs
         self.weight_init = weight_init
         self.layers = []
         
@@ -152,3 +158,100 @@ class NeuralNetwork:
         for layer in self.layers:
             x = layer.forward(x)
         return x
+    
+    def backward(self, y_real: npt.NDArray, y_pred: npt.NDArray):
+        L = len(self.layers)
+        db_list = deque()
+        dW_list = deque()
+        # Last layer's del_L/del_a for multiclass classification
+        # Assuming that y_real will be one-hot vector for that class label
+        da = y_pred - y_real
+        
+        for k in range(L-1, -1, -1):
+            db = da # del_L/del_b for this layer
+            db_list.appendleft(db)
+
+            h_prev_layer = self.layers[k-1].h if k>0 else self.layers[k].x # previous layer's output is the initial input x if it is the first layer
+            
+            dW = da @ h_prev_layer.T # del_L_del_W for this layer
+            dW_list.appendleft(dW)
+
+            if k == 0:
+                # since done computing dW and db for first (hidden) layer
+                # no need to compute dh and and using that to
+                # compute da for previous layer which is the input itself
+                break
+
+            # compute del_L/del_h for next iteration (/of previous layer)
+            dh = self.layers[k].W.T @ da
+            
+            # compute del_L/del_a of previous layer
+            da = dh * self.layers[k-1].activation.grad(self.layers[k-1].a)
+        
+        return (dW_list, db_list)
+    
+    def train(self, X_train: npt.NDArray, Y_train:npt.NDArray, batch_size = 10, epochs=100):
+        # Expect X_train.shape[0] = Y_train.shape[0] to be the number of examples
+
+        for epoch in range(epochs):
+            num_examples_seen = 0
+            total_loss = 0
+            L = 0
+            total_db_list = deque()
+            total_dW_list = deque()
+            W_optimizers = deque()
+            b_optimizers = deque()
+
+            # Initialize the total parameter change variables
+            for layer in self.layers:
+                L += 1
+                total_db_list.append(np.zeros_like(layer.b))
+                total_dW_list.append(np.zeros_like(layer.W))
+                W_optimizers.append(Optimizer(theta=layer.W,
+                                              learning_rate=self.lr,
+                                              type=self.opt_name,
+                                              **self.optimizer_params))
+                b_optimizers.append(Optimizer(theta=layer.b,
+                                              learning_rate=self.lr,
+                                              type=self.opt_name,
+                                              **self.optimizer_params))
+            
+            # Pass over the training data once
+            for x, y_real in zip(X_train, Y_train):
+                # Forward pass to find y_pred
+                y_pred = self.forward(x)
+                assert (y_real.shape == y_pred.shape), "The training could not start as the prediction shape does not match true output."
+
+                # Accumulate this example's loss
+                total_loss += losses.ce(y_real, y_pred)
+
+                # Backpropagate to find dW and db for all layers for current example
+                curr_dW_list, curr_db_list = self.backward(y_real, y_pred)
+
+                # Accumulate this into total dW and db per batch
+                for l in range(L):
+                    total_db_list[l] += curr_db_list[l]
+                    total_dW_list[l] += curr_dW_list[l]
+                
+                num_examples_seen += 1
+
+                # If a batch of example worth dW and db has been accumulated, take an optimization step
+                if num_examples_seen%batch_size == 0:
+                    # Take a optimization step
+                    for l in range(L):
+                        b_optimizers[l].update(total_db_list[l])
+                        W_optimizers[l].update(total_dW_list[l])
+                        # Reset the total dW and db for next batch
+                        total_db_list[l] *= 0.0
+                        total_dW_list[l] *= 0.0
+            print(f"Epoch {epoch+1}, Loss = {total_loss}")
+            # if epoch % 10 == 0:
+            #     print(f"Epoch {epoch+1}, Loss = {total_loss}")
+                        
+
+
+
+                
+            
+                
+

optimizers.py
@@ -8,11 +8,10 @@ class Optimizer():
             raise ValueError(f"Unknown optimizer type: {type}, choose from: {self._optimizer_choices}")
         
         # Initiate theta_0
-        self.theta = np.copy(theta) if isinstance(theta, np.ndarray) else float(theta)
+        self.theta = theta
         self.type = type
         self.learning_rate = learning_rate
         self.extra_params = kwargs
-        print(self.extra_params)
 
         # Initiate v_0 = 0
         self.v = np.zeros_like(self.theta) if isinstance(self.theta, np.ndarray) else 0.0